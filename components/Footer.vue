88e65437c198c65b8394b2fea008ca3c8bc09b7d
William <william.chou@u-ark.com>
2023-10-30 17:12:53 +0800

    footer api

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,10 +18,13 @@
 						/>
 					</div>
 					<ul class="my-4 flex text-white justify-center text-sm md:hidden">
-						<li class="px-3 border-r border-white">關於我們</li>
-						<li class="px-3 border-r border-white">購物流程</li>
-						<li class="px-3 border-r border-white">會員中心</li>
-						<li class="px-3">客服中心</li>
+						<li
+							v-for="field in footerList"
+							:key="field.field_id"
+							class="px-3 border-r border-white last:border-none"
+						>
+							{{ field.field_value }}</li
+						>
 					</ul>
 					<div class="text-center text-[10px] md:mt-4 md:text-sm">
 						<p>統一編號: 27731537</p>
@@ -35,36 +38,19 @@
 					</div>
 				</div>
 				<div class="hidden md:flex flex-1 text-[13px]">
-					<dl class="flex-1 text-left">
-						<dt class="text-[18px] font-bold">關於我們</dt>
-						<dd class="my-1">春天官網</dd>
-						<dd class="my-1">品牌精神</dd>
-						<dd class="my-1">門市當月活動</dd>
-						<dd class="my-1">門市據點</dd>
-						<dd class="my-1">人才招募</dd>
-					</dl>
-					<dl class="flex-1 text-left">
-						<dt class="text-[18px] font-bold">購物流程</dt>
-						<dd class="my-1">購物說明</dd>
-						<dd class="my-1">運費及配送</dd>
-						<dd class="my-1">免責聲明</dd>
-						<dd class="my-1">付款方式</dd>
-					</dl>
-					<dl class="flex-1 text-left">
-						<dt class="text-[18px] font-bold">會員專區</dt>
-						<dd class="my-1">註冊流程</dd>
-						<dd class="my-1">會員制度</dd>
-						<dd class="my-1">會員點數使用規則</dd>
-						<dd class="my-1">隱私權政策</dd>
-						<dd class="my-1">會員服務條款</dd>
-					</dl>
-					<dl class="flex-1 text-left">
-						<dt class="text-[18px] font-bold">客服中心</dt>
-						<dd class="my-1">退換貨須知</dd>
-						<dd>問題Q&A</dd>
-						<dd class="my-1">防詐騙宣導</dd>
-						<dd class="my-1">與我們聯繫</dd>
-						<dd class="my-1">大宗採購/新品提報</dd>
+					<dl
+						v-for="field in footerList"
+						:key="field.field_id"
+						class="flex-1 text-left"
+					>
+						<dt class="text-[18px] font-bold">{{ field.field_value }}</dt>
+						<dd
+							v-for="item in field.field_items"
+							:key="item.content_id"
+							class="my-1"
+						>
+							{{ item.content_name }}</dd
+						>
 					</dl>
 				</div>
 			</div>
@@ -77,6 +63,16 @@
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getFooterInfoApi } from '@/api/footer';
+const footerList = ref([]);
+
+const getFooter = async () => {
+	const data = await getFooterInfoApi();
+	footerList.value = data.result;
+	console.log(data);
+};
+await getFooter();
+</script>
 
 <style lang="scss" scoped></style>
