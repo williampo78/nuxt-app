f266c141eaee857a0a6c2d277e192af6d24b0c74
William <william.chou@u-ark.com>
2023-11-01 10:20:18 +0800

    api config

--- composables/api.ts
+++ composables/api.ts
@@ -21,42 +21,40 @@ interface HttpParms {
  * @param obj 请求参数
  * @returns 响应结果
  */
-export const service = (obj: HttpParms) => {
-    const res = new Promise<any>((resolve, reject) => {
-        const config = useRuntimeConfig()
-        const BASEURL: string = config.public.baseURL;
-        useFetch(
-            BASEURL + '/api' + obj.url,
-            {
-                method: obj.method ?? "GET",
-                query: obj?.query ?? null,
-                body: obj?.body ?? null,
-                onRequest({ request, options }) {
+export const service = async (obj: HttpParms) => {
+    const config = useRuntimeConfig()
+    const BASEURL: string = config.public.baseURL;
+    const { data } = await useFetch<any>(
+        BASEURL + '/api' + obj.url,
+        {
+            method: obj.method ?? "GET",
+            query: obj?.query ?? null,
+            body: obj?.body ?? null,
+            onRequest({ request, options }) {
 
-                    // 设置请求报头
-                    options.headers = options.headers || {}
-                    /**如果接口需求携带token请求，则可先自行使用官方的useCookie()方法设置Cookie存储后，再使用useCookie()方法，取出token使用。如下例子：*/
-                    //const token = useCookie('token')
-                    //@ts-ignore
-                    //options.headers.Authorization = token.value||null
-                },
-                onRequestError({ request, options, error }) {
-                    // 处理请求错误
-                    console.log("服务器链接失败!")
-                    reject(error)
-                },
-                onResponse({ request, response, options }) {
-                    // 处理响应数据
-                    resolve(response._data)
-                },
-                onResponseError({ request, response, options }) {
-                    // 处理响应错误
-                }
+                // 设置请求报头
+                options.headers = options.headers || {}
+                /**如果接口需求携带token请求，则可先自行使用官方的useCookie()方法设置Cookie存储后，再使用useCookie()方法，取出token使用。如下例子：*/
+                //const token = useCookie('token')
+                //@ts-ignore
+                //options.headers.Authorization = token.value||null
             },
+            onRequestError({ request, options, error }) {
+                // 处理请求错误
+                console.log("服务器链接失败!")
+                return Promise.reject(error)
+            },
+            onResponse({ request, response, options }) {
+                // 处理响应数据
 
-        )
-    })
-    return res;
+                return Promise.resolve(response._data)
+            },
+            onResponseError({ request, response, options, error }) {
+                // 处理响应错误
 
+                return Promise.reject(response._data)
+            }
+        })
+    return data.value
 }
 
