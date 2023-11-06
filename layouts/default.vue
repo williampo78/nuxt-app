51a37df2c46e0d986385d0b9c59262583209fc4a
William <william.chou@u-ark.com>
2023-11-06 17:21:05 +0800

    scroll to top button

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,11 +12,24 @@
 		</main>
 	</div>
 	<Footer />
+	<button
+		@click="scrollToTop"
+		class="w-[50px] h-[50px] text-white flex flex-col justify-center items-center rounded-full bg-blue-primary border-2 border-white bottom-10 right-10 fixed z-20"
+	>
+		<span class="text-sm"
+			><font-awesome-icon :icon="['fas', 'chevron-up']"
+		/></span>
+		<span class="text-xs">TOP</span>
+	</button>
 </template>
 
 <script setup>
 const modalStore = useModal();
 const route = useRoute();
+
+const scrollToTop = () => {
+	window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+};
 </script>
 
 <style lang="scss" scoped></style>
