89ed486b3e761c741d007c9bffe444ea7609a84c
William <william.chou@u-ark.com>
2024-03-07 15:26:25 +0800

    商品加入購物車時若無登入自動導到登入頁

--- store/collection.ts
+++ store/collection.ts
@@ -20,19 +20,31 @@ export const useCollection = defineStore('collection', {
         },
         async toggleColllection(payload: { id: number | string, isCollected: boolean }) {
             const modalStore = useModal()
+            const token = useCookie('token')
+            const router = useRouter()
+            if (!token.value) {
+                modalStore.openModal({
+                    type: 'toast',
+                    message: '請先登入',
+                    icon: 'warning'
+                });
+                router.push('/login')
+                return
+            }
+            try {
+                await updateCollectionsApi({
+                    product_id: payload.id,
+                    status: payload.isCollected ? -1 : 0,
+                });
 
-            await updateCollectionsApi({
-                product_id: payload.id,
-                status: payload.isCollected ? -1 : 0,
-            });
+                await this.getCollections();
 
-            await this.getCollections();
-
-            modalStore.openModal({
-                type: 'toast',
-                message: payload.isCollected ? '已取消收藏' : '已加入收藏',
-                icon: payload.isCollected ? 'uncollect' : 'collect'
-            });
+                modalStore.openModal({
+                    type: 'toast',
+                    message: payload.isCollected ? '已取消收藏' : '已加入收藏',
+                    icon: payload.isCollected ? 'uncollect' : 'collect'
+                });
+            } catch (error) { }
         }
     },
 });
